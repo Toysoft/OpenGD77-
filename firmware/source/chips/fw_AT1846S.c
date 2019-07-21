133debbc19261c5cbb75190c6d6058c04388e4c8
Kai Ludwig <email@dg4klu.de>
2019-07-21 09:59:09 +0200

    added DMR mode specific modu_det_sel (SQ setting) to AT1846DMRSettings

--- firmware/source/chips/fw_AT1846S.c
+++ firmware/source/chips/fw_AT1846S.c
@@ -163,7 +163,8 @@ const uint8_t AT1846DMRSettings[][AT1846_BYTES_PER_COMMAND] = {
 		{0x41, 0x47, 0x31}, // Digital voice gain, (bits 6:0) however default value is supposed to be 0x4006 hence some bits are being set outside the documented range
 		{0x42, 0x10, 0x36}, // RDA1846 lists this as Vox Shut threshold
 		{0x58, 0xBC, 0xFD}, // Disable all filters in DMR mode
-		{0x44, 0x06, 0xCC} // set internal volume to 80%
+		{0x44, 0x06, 0xCC}, // set internal volume to 80%
+		{0x3a, 0x40, 0xC2}// modu_det_sel (SQ setting)
 		};
 
 void I2C_AT1846S_send_Settings(const uint8_t settings[][3],int numSettings)
