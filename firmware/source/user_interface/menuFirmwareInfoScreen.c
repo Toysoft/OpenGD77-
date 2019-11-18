51ad7b3a95a345bda5c1c6fe7c7907275b1a48f5
Roger Clark <roger@rogerclark.net>
2019-11-18 21:36:23 +1100

    Changed firmware info screen to remove VK3KYY text and add build time

--- firmware/source/user_interface/menuFirmwareInfoScreen.c
+++ firmware/source/user_interface/menuFirmwareInfoScreen.c
@@ -40,9 +40,10 @@ int menuFirmwareInfoScreen(int buttons, int keys, int events, bool isFirstRun)
 static void updateScreen()
 {
 	UC1701_clearBuf();
-	UC1701_printCentered(12, "OpenGD77",UC1701_FONT_8x16);
-	UC1701_printCentered(32,(char *)FIRMWARE_VERSION_STRING,UC1701_FONT_8x16);
-	UC1701_printCentered(48,__DATE__,UC1701_FONT_8x16);
+	UC1701_printCentered(8, "OpenGD77",UC1701_FONT_8x16);
+	UC1701_printCentered(34, "Built",UC1701_FONT_8x8);
+	UC1701_printCentered(44,__TIME__,UC1701_FONT_8x8);
+	UC1701_printCentered(54,__DATE__,UC1701_FONT_8x8);
 	UC1701_render();
 	displayLightTrigger();
 }
