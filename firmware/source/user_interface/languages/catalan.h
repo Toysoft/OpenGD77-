56ea54557b0eca134bbd0b67216c7282bf088418
MarthG <cgcarlos444@gmail.com>
2019-11-28 22:24:36 +0100

    Update catalan.h

--- firmware/source/user_interface/languages/catalan.h
+++ firmware/source/user_interface/languages/catalan.h
@@ -1,3 +1,25 @@
+/*
+ * Copyright (C)2019 Roger Clark. VK3KYY / G4KYF
+ *
+ * Using some code ported from MMDVM_HS by Andy CA6JAU
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#ifndef USER_INTERFACE_LANGUAGES_CATALAN_H_
+#define USER_INTERFACE_LANGUAGES_CATALAN_H_
+
 const stringsTable_t catalanLanguage =
 {
 	.LANGUAGE_NAME             = "Catala",
@@ -83,3 +105,5 @@ const stringsTable_t catalanLanguage =
 	.key_repeat			= "Key rpt:%1d.%1ds",
 	.dmr_filter_timeout	= "Filtre temps",
 };
+
+#endif /* USER_INTERFACE_LANGUAGES_CATALAN_H_*/
