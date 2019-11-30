c6f9680c2a1084cb17ded338b28ea297b92b9d9e
Daniel Caujolle-Bert <f1rmb.daniel@gmail.com>
2019-11-30 06:51:18 +0100

    Forgotten file...

---
+++ firmware/source/user_interface/languages/french.h
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C)2019 Roger Clark. VK3KYY / G4KYF
+ *
+ * Using some code ported from MMDVM_HS by Andy CA6JAU
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#ifndef USER_INTERFACE_LANGUAGES_FRENCH_H_
+#define USER_INTERFACE_LANGUAGES_FRENCH_H_
+
+const stringsTable_t frenchLanguage =
+{
+.LANGUAGE_NAME			= "Français",
+.language			= "Langue",
+.menu				= "Menu",
+.credits			= "Crédits",
+.zone				= "Zone",
+.rssi				= "RSSI",
+.battery			= "Batterie",
+.contacts			= "Contacts",
+.firmware_info			= "Info Firmware",
+.last_heard			= "Derniers reçus",
+.options			= "Options",
+.display_options		= "Options aff.",
+.channel_details		= "Détails canal",
+.new_contact			= "Nouv. contact",
+.contact_list			= "Liste contacts",
+.hotspot_mode			= "Mode hotspot",
+.contact_details		= "Détails contact",
+.batteryVoltage			= "%d.%dV",
+.built				= "Créé",
+.zones				= "Zones",
+.keypad				= "Clavier",
+.ptt				= "PTT",
+.locked				= "Vérrouillé",
+.press_blue_plus_star		= "Pressez Bleu + *",
+.to_unlock			= "pour dévérouiller",
+.unlocked			= "Dévérouillé",
+.power_off			= "Extinction...",
+.error				= "ERREUR",
+.rx_only			= "Rx uniqmnt.",
+.out_of_band			= "HORS BANDE",
+.timeout			= "TIMEOUT",
+.tg_entry			= "Entrez TG",
+.pc_entry			= "Entrez PC",
+.user_dmr_id			= "DMR ID perso.",
+.contact 			= "Contact",
+.displayRxFreq			= "%d.%05d MHz",
+.accept_call			= "Accepter appel ?",
+.private_call			= "Appel privé",
+.squelch			= "Silencieux",
+.vfoRxFreq			= "%cR %d.%05d MHz",
+.vfoTxFreq			= "%cT %d.%05d MHz",
+.vfoEnterFreq			= "%c%c%c.%c%c%c%c%c MHz",
+.quick_menu 			= "Menu rapide",
+.filter				= "Filtre:%s",
+.all_channels			= "Tous Canaux",
+.gotoChannel			= "Aller %d",
+.scan				= "Scan",
+.channelToVfo			= "Canal --> VFO",
+.vfoToChannel			= "VFO --> Canal",
+.group				= "Groupe",
+.private			= "Privé",
+.all				= "Tous",
+.type				= "Type:",
+.timeSlot			= "Timeslot",
+.none				= "aucun",
+.contact_saved			= "Contact savvé",
+.duplicate			= "Dupliqué",
+.tg				= "TG",
+.pc				= "PC",
+.ts				= "TS",
+.mode				= "Mode",
+.colour_code			= "Code Couleur",
+.n_a				= "ND",
+.channelDetailsRx		= "Rx:%d.%05dMHz",
+.channelDetailsTx		= "Tx:%d.%05dMHz",
+.bandwidth			= "Excurs.",
+.stepFreq			= "Pas:%d.%02dkHz",
+.tot				= "TOT",
+.off				= "Off",
+.zone_skip			= "Saut Zone",
+.all_skip			= "Saut Compl.",
+.yes				= "Oui",
+.no				= "Non",
+.rx_group			= "Grp Rx",
+.on				= "On",
+.timeout_beep			= "Son timeout",
+.factory_reset			= "Ràz Usine",
+.calibration			= "Alignement",
+.band_limits			= "Lim. Bandes",
+.beep_volume			= "Vol. bip",
+.dmr_mic_gain			= "DMR mic",
+.key_long			= "Appui long:%1d.%1ds",
+.key_repeat			= "Répét.:%1d.%1ds",
+.dmr_filter_timeout		= "Tps filtre",
+.brightness			= "Rétro écl.",
+.contrast			= "Contraste",
+.colour_invert			= "Couleur:Inverse",
+.colour_normal			= "Couleur:Normale",
+.backlight_timeout		= "Timeout",
+.scan_delay			= "Délai scan",
+};
+#endif /* USER_INTERFACE_LANGUAGES_FRENCH_H_ */
