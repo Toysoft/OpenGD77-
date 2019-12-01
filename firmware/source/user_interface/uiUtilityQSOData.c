d917da47a617af574812284726727fc2496cfb81
Alex Ernst <dl4lex@t-online.de>
2019-12-01 14:43:00 +0100

    Change keycodes in preparation for alpha-numeric input.

--- firmware/source/user_interface/uiUtilityQSOData.c
+++ firmware/source/user_interface/uiUtilityQSOData.c
@@ -684,45 +684,12 @@ void setOverrideTGorPC(int tgOrPc, bool privateCall) {
 
 char keypressToNumberChar(int keys)
 {
-	if (KEYCHECK_PRESS(keys,KEY_0))
-	{
-		return '0';
-	}
-	else if (KEYCHECK_PRESS(keys,KEY_1))
-	{
-		return '1';
-	}
-	else if (KEYCHECK_PRESS(keys,KEY_2))
-	{
-		return '2';
-	}
-	else if (KEYCHECK_PRESS(keys,KEY_3))
-	{
-		return '3';
-	}
-	else if (KEYCHECK_PRESS(keys,KEY_4))
-	{
-		return '4';
-	}
-	else if (KEYCHECK_PRESS(keys,KEY_5))
-	{
-		return '5';
-	}
-	else if (KEYCHECK_PRESS(keys,KEY_6))
-	{
-		return '6';
-	}
-	else if (KEYCHECK_PRESS(keys,KEY_7))
-	{
-		return '7';
-	}
-	else if (KEYCHECK_PRESS(keys,KEY_8))
-	{
-		return '8';
-	}
-	else if (KEYCHECK_PRESS(keys,KEY_9))
-	{
-		return '9';
+	char keycode = KEYCHAR(keys);
+	if (KEYCHECK_MOD(keys, KEY_MOD_PRESS, KEY_MOD_PRESS)) {
+		if (keycode >= '0' && keycode <= '9')
+		{
+			return keycode;
+		}
 	}
 	return '\0';
 }
