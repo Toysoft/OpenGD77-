8c8dac70954956007337ffad382e0a71e9051662
Roger Clark <roger@rogerclark.net>
2019-12-02 08:14:50 +1100

    Added Danish language file, but some texts are too long. Currently waiting for an update from OZ1MAX

---
+++ firmware/source/user_interface/languages/danish.h
@@ -0,0 +1,136 @@
+/* -*- coding: windows-1252-unix; -*- */
+/*
+ * Copyright (C)2019 Roger Clark. VK3KYY / G4KYF
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#ifndef USER_INTERFACE_LANGUAGES_Dansk_H_
+#define USER_INTERFACE_LANGUAGES_Dansk_H_
+/********************************************************************
+ *
+ * VERY IMPORTANT.
+ * This file should not be saved with UTF-8 encoding
+ * Use Notepad++ on Windows with ANSI encoding
+ * or emacs on Linux with windows-1252-unix encoding
+ *
+ ********************************************************************/
+const stringsTable_t danskLanguage =
+{
+.LANGUAGE_NAME 			= "Dansk",
+.language				= "Sprog",
+.menu					= "Menu",
+.credits				= "Credits",
+.zone					= "Zone",
+.rssi					= "RSSI",
+.battery				= "Batteri",
+.contacts				= "Kontakter",
+.firmware_info			= "Firmware info",
+.last_heard				= "Sidst Hord",
+.options				= "Valg",
+.display_options		= "Display Valg",
+.channel_details		= "Kanal detaljer",
+.new_contact			= "Ny Kontakt",
+.contact_list			= "Kontakt liste",
+.hotspot_mode			= "Hotspot mode",
+.contact_details		= "Kontakt Detaljer",
+.batteryVoltage			= "%d.%dV",
+.built					= "Version",
+.zones					= "Zoner",
+.keypad					= "Keypad",
+.ptt					= "PTT",
+.locked					= "Laast",
+.press_blue_plus_star	= "Tryk Blaa +*",
+.to_unlock				= "for at oplase",
+.unlocked				= "Oplast",
+.power_off				= "Slukker nu...",
+.error					= "FEJL",
+.rx_only				= "Kun Rx",
+.out_of_band			= "Ude af FRQ",
+.timeout				= "TIMEOUT",
+.tg_entry				= "Indtast TG",
+.pc_entry				= "Indtast PC",
+.user_dmr_id			= "Bruger DMR ID",
+.contact 				= "Kontakt",
+.displayRxFreq			= "%d.%05d MHz",
+.accept_call			= "Modtag kald?",
+.private_call			= "Privat kald",
+.squelch				= "Squelch",
+.vfoRxFreq				= "%cR %d.%05d MHz",
+.vfoTxFreq				= "%cT %d.%05d MHz",
+.vfoEnterFreq			= "%c%c%c.%c%c%c%c%c MHz",
+.quick_menu 			= "Quick Menu",
+.filter					= "Filter:%s",
+.all_channels			= "Alle kanaler",
+.gotoChannel			= "Goto %d",
+.scan					= "Scan",
+.channelToVfo			= "Kanal --> VFO",
+.vfoToChannel			= "VFO --> Kanal",
+.group					= "Gruppe",
+.private				= "Privat",
+.all					= "Alle",
+.type					= "Type:",
+.timeSlot				= "Timeslot",
+.none					= "Ingen",
+.contact_saved			= "Kontakt Gemt",
+.duplicate				= "Duplet",
+.tg						= "TG",
+.pc						= "PC",
+.ts						= "TS",
+.mode					= "Mode",
+.colour_code			= "Color Code",
+.n_a					= "N/A",
+.channelDetailsRx		= "Rx:%d.%05dMHz",
+.channelDetailsTx		= "Tx:%d.%05dMHz",
+.bandwidth				= "Bandwidth",
+.stepFreq				= "Step:%d.%02dkHz",
+.tot					= "TOT",
+.off					= "Fra",
+.zone_skip				= "Zone Skip",
+.all_skip				= "Alle Skip",
+.yes					= "Ja",
+.no						= "Nej",
+.rx_group				= "Rx Grp",
+.on						= "On",
+.timeout_beep			= "Timeout bip",
+.factory_reset			= "Fabriksinstil",
+.calibration			= "Kalibrering",
+.band_limits			= "Frekvens omrade",
+.beep_volume			= "Bip vol",
+.dmr_mic_gain			= "DMR mic",
+.key_long				= "Key long:%1d.%1ds",
+.key_repeat				= "Key rpt:%1d.%1ds",
+.dmr_filter_timeout		= "Filter time",
+.brightness				= "Lys styrke",
+.contrast				= "Contrast",
+.colour_invert			= "Farve:Sort",
+.colour_normal			= "Farve:Normal",
+.backlight_timeout		= "Timeout",
+.scan_delay				= "Scan delay",
+.YES					= "JA",
+.NO						= "NEJ",
+.DISMISS				= "FORTRYD",
+.scan_mode				= "Scan mode",
+.hold					= "Hold",
+.pause					= "Pause",
+};
+/********************************************************************
+ *
+ * VERY IMPORTANT.
+ * This file should not be saved with UTF-8 encoding
+ * Use Notepad++ on Windows with ANSI encoding
+ * or emacs on Linux with windows-1252-unix encoding
+ *
+ ********************************************************************/
+#endif /* USER_INTERFACE_LANGUAGES_Dansk_H_ */
