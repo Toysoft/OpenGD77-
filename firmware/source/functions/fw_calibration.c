81d27fb7bd8a90eaf8347df7da1a8717d9a3d7b9
g4eml <colin@durbridge.me.uk>
2019-12-12 21:24:35 +0000

    Add comments for calibration values.

--- firmware/source/functions/fw_calibration.c
+++ firmware/source/functions/fw_calibration.c
@@ -28,9 +28,9 @@ const int NUM_PA_DRIVE_INDEXES = 20;// not including zero
 calibrationStruct_t calibrationVHF;
 calibrationStruct_t calibrationUHF;
 */
-void read_val_DACDATA_shift(int offset, uint8_t* val_shift)
-{
-	uint8_t buffer[1];
+void read_val_DACDATA_shift(int offset, uint8_t* val_shift)						//Sent to HRC6000 register 0x37. This sets the DMR received Audio Gain. (And Beep Volume)
+{																				//Effective for DMR Receive Only.
+	uint8_t buffer[1];															//only the low 4 bits are used. 1.5dB change per increment 0-31
 	SPI_Flash_read(EXT_DACDATA_shift+offset,buffer,1);
 
 	*val_shift=buffer[0]+1;
@@ -41,64 +41,64 @@ void read_val_DACDATA_shift(int offset, uint8_t* val_shift)
 	*val_shift|=0x80;
 }
 
-void read_val_twopoint_mod(int offset, uint8_t* val_0x47, uint8_t* val_0x48)
-{
-	uint8_t buffer[2];
+void read_val_twopoint_mod(int offset, uint8_t* val_0x47, uint8_t* val_0x48)      // Sent to HRC6000 register 0x47 and 0x48 to set the DC level of the Mod1 output. This sets the frequency of the AT1846 Reference Oscillator.
+{																				  // Effective for DMR and FM Receive and Transmit. Calibrates the receive and transmit frequencies.
+	uint8_t buffer[2];															  // Only one setting should be required for all frequencies but the Cal table has separate entries for VHF and UHF.
 	SPI_Flash_read(EXT_twopoint_mod+offset,buffer,2);
 	*val_0x47=buffer[0];
 	*val_0x48=buffer[1];
 }
 
-void read_val_Q_MOD2_offset(int offset, uint8_t* val_0x04)
-{
-	uint8_t buffer[1];
-	SPI_Flash_read(EXT_Q_MOD2_offset+offset,buffer,1);
+void read_val_Q_MOD2_offset(int offset, uint8_t* val_0x04)						  //Sent to the HRC6000 register 0x04 which is the offset value for the Mod 2 output.
+{																				  //However according to the schematics the Mod 2 output is not connected.
+	uint8_t buffer[1];															  //Therefore the function of this setting is unclear. (possible datasheet error?)
+	SPI_Flash_read(EXT_Q_MOD2_offset+offset,buffer,1);							  //The Cal table has one entry for each band and this is almost identical to 0x47 above.
 	*val_0x04=buffer[0];
 }
 
-void read_val_phase_reduce(int offset, uint8_t* val_0x46)
-{
-	uint8_t buffer[1];
-	SPI_Flash_read(EXT_phase_reduce+offset,buffer,1);
+void read_val_phase_reduce(int offset, uint8_t* val_0x46)						  //Sent to the HRC6000 register 0x46. This adjusts the level of the DMR Modulation on the MOD1 output.
+{																				  //Mod 1 controls the AT1846 Reference oscillator so this is effectively the deviation setting for DMR.
+	uint8_t buffer[1];															  //Because it modulates the reference oscillator the deviation will change depending on the frequency being used.
+	SPI_Flash_read(EXT_phase_reduce+offset,buffer,1);							  //Only affects DMR Transmit. The Cal table has 8 calibration values for each band.
 	*val_0x46=buffer[0];
 }
 
-void read_val_pga_gain(int offset, uint8_t* value)
+void read_val_pga_gain(int offset, uint8_t* value)								  //Sent to AT1846S register 0x0a bits 6-10. Sets Voice Analogue Gain for FM Transmit.
 {
 	uint8_t buffer[1];
 	SPI_Flash_read(EXT_pga_gain+offset,buffer,1);
 	*value=buffer[0] & 0x1f;
 }
 
-void read_val_voice_gain_tx(int offset, uint8_t* value)
+void read_val_voice_gain_tx(int offset, uint8_t* value)							  //Sent to AT1846S register 0x41 bits 0-6. Sets Voice Digital Gain for FM Transmit.
 {
 	uint8_t buffer[1];
 	SPI_Flash_read(EXT_voice_gain_tx+offset,buffer,1);
 	*value=buffer[0] & 0x7f;
 }
 
-void read_val_gain_tx(int offset, uint8_t* value)
+void read_val_gain_tx(int offset, uint8_t* value)   							//Sent to AT1846S register 0x44 bits 8-11. Sets Voice Digital Gain after ADC for FM Transmit.
 {
 	uint8_t buffer[1];
 	SPI_Flash_read(EXT_gain_tx+offset,buffer,1);
 	*value=buffer[0] & 0x0f;
 }
 
-void read_val_padrv_ibit(int offset, uint8_t* value)
+void read_val_padrv_ibit(int offset, uint8_t* value)							//Sent to AT1846S register 0x0a bits 11-14. Sets PA Power Control for DMR and FM Transmit.
 {
 	uint8_t buffer[1];
 	SPI_Flash_read(EXT_padrv_ibit+offset,buffer,1);
 	*value=buffer[0] & 0x0f;
 }
 
-void read_val_xmitter_dev_wideband(int offset, uint16_t* value)
+void read_val_xmitter_dev_wideband(int offset, uint16_t* value)					//Sent to AT1846S register 0x59 bits 6-15. Sets Deviation for Wideband FM Transmit
 {
 	uint8_t buffer[2];
 	SPI_Flash_read(EXT_xmitter_dev_wideband+offset,buffer,2);
 	*value=buffer[0] + ((buffer[1] & 0x03) << 8);
 }
 
-void read_val_xmitter_dev_narrowband(int offset, uint16_t* value)
+void read_val_xmitter_dev_narrowband(int offset, uint16_t* value)				//Sent to AT1846S register 0x59 bits 6-15. Sets Deviation for NarrowBand FM Transmit
 {
 	uint8_t buffer[2];
 	SPI_Flash_read(EXT_xmitter_dev_narrowband+offset,buffer,2);
@@ -109,7 +109,7 @@ void read_val_dev_tone(int index, uint8_t *value)
 {
 	int address;
 
-	if (trxCurrentBand[TRX_TX_FREQ_BAND] == RADIO_BAND_UHF)
+	if (trxCurrentBand[TRX_TX_FREQ_BAND] == RADIO_BAND_UHF)                    //Sent to AT1846S register 0x59 bits 0-5. Sets Tones Deviation for FM Transmit
 	{
 		address = EXT_uhf_dev_tone+index;
 	}
@@ -120,64 +120,64 @@ void read_val_dev_tone(int index, uint8_t *value)
 	SPI_Flash_read(address, value, 1);
 }
 
-void read_val_dac_vgain_analog(int offset, uint8_t* value)
+void read_val_dac_vgain_analog(int offset, uint8_t* value)					//Sent to AT1846S register 0x44 bits 0-3. Sets Digital Audio Gain for FM and DMR Receive.
 {
 	uint8_t buffer[1];
 	SPI_Flash_read(EXT_dac_vgain_analog+offset,buffer,1);
 	*value=buffer[0] & 0x0f;
 }
 
-void read_val_volume_analog(int offset, uint8_t* value)
+void read_val_volume_analog(int offset, uint8_t* value)						//Sent to AT1846S register 0x44 bits 4-7. Sets Analogue Audio Gain for FM and DMR Receive.
 {
 	uint8_t buffer[1];
 	SPI_Flash_read(EXT_volume_analog+offset,buffer,1);
 	*value=buffer[0] & 0x0f;
 }
 
-void read_val_noise1_th_wideband(int offset, uint16_t* value)
+void read_val_noise1_th_wideband(int offset, uint16_t* value)				//Sent to AT1846S register 0x48. Sets Noise 1 Threshold  for FM Wideband Receive.
 {
 	uint8_t buffer[2];
 	SPI_Flash_read(EXT_noise1_th_wideband+offset,buffer,2);
 	*value=((buffer[0] & 0x7f) << 7) + ((buffer[1] & 0x7f) << 0);
 }
 
-void read_val_noise2_th_wideband(int offset, uint16_t* value)
+void read_val_noise2_th_wideband(int offset, uint16_t* value)				//Sent to AT1846S register 0x60. Sets Noise 2 Threshold  for FM  Wideband Receive.
 {
 	uint8_t buffer[2];
 	SPI_Flash_read(EXT_noise2_th_wideband+offset,buffer,2);
 	*value=((buffer[0] & 0x7f) << 7) + ((buffer[1] & 0x7f) << 0);
 }
 
-void read_val_rssi3_th_wideband(int offset, uint16_t* value)
+void read_val_rssi3_th_wideband(int offset, uint16_t* value)				//Sent to AT1846S register 0x3F. Sets RSSI3 Threshold  for  Wideband Receive.
 {
 	uint8_t buffer[2];
 	SPI_Flash_read(EXT_rssi3_th_wideband+offset,buffer,2);
 	*value=((buffer[0] & 0x7f) << 7) + ((buffer[1] & 0x7f) << 0);
 }
 
-void read_val_noise1_th_narrowband(int offset, uint16_t* value)
+void read_val_noise1_th_narrowband(int offset, uint16_t* value)				//Sent to AT1846S register 0x48. Sets Noise 1 Threshold  for FM Narrowband Receive.
 {
 	uint8_t buffer[2];
 	SPI_Flash_read(EXT_noise1_th_narrowband+offset,buffer,2);
 	*value=((buffer[0] & 0x7f) << 7) + ((buffer[1] & 0x7f) << 0);
 }
 
-void read_val_noise2_th_narrowband(int offset, uint16_t* value)
+void read_val_noise2_th_narrowband(int offset, uint16_t* value)				//Sent to AT1846S register 0x60. Sets Noise 2 Threshold  for FM  Narrowband  Receive.
 {
 	uint8_t buffer[2];
 	SPI_Flash_read(EXT_noise2_th_narrowband+offset,buffer,2);
 	*value=((buffer[0] & 0x7f) << 7) + ((buffer[1] & 0x7f) << 0);
 }
 
-void read_val_rssi3_th_narrowband(int offset, uint16_t* value)
+void read_val_rssi3_th_narrowband(int offset, uint16_t* value)				//Sent to AT1846S register 0x3F. Sets RSSI3 Threshold  for  Narrowband Receive.
 {
 	uint8_t buffer[2];
 	SPI_Flash_read(EXT_rssi3_th_narrowband+offset,buffer,2);
 	*value=((buffer[0] & 0x7f) << 7) + ((buffer[1] & 0x7f) << 0);
 }
 
-void read_val_squelch_th(int offset, int mod, uint16_t* value)
-{
+void read_val_squelch_th(int offset, int mod, uint16_t* value)				//Sent to At1846S register 0x49. sets squelch open threshold for FM Receive.
+{																			//8 Cal Table Entries for each band. Frequency Dependent.
 	uint8_t buffer[1];
 	SPI_Flash_read(EXT_squelch_th+offset,buffer,1);
 	uint8_t v1 = buffer[0]-mod;
@@ -190,17 +190,11 @@ void read_val_squelch_th(int offset, int mod, uint16_t* value)
 	*value=(v1 << 7) + (v2 << 0);
 }
 
-bool calibrationGetPowerForFrequency(int freq, calibrationPowerValues_t *powerSettings)
-{
-	/*
-	 * Power for
-	 * UHF is in 5Mhz bands starting at 400Mhz.
-	 * VHF is in 5Mhz bands from 135Mhz
-	 * first byte of each value is the low power and the second byte is the high power value
-	 */
-	const uint32_t POWER_CALIBRATION_ADDRESS_UHF_400MHZ = 0x8F00B;
-	const uint32_t POWER_CALIBRATION_ADDRESS_VHF_135MHZ = 0x8F07B;
-	int address;
+bool calibrationGetPowerForFrequency(int freq, calibrationPowerValues_t *powerSettings)				//Power Settings for Transmit
+{																							        //16 Entries of two bytes each.
+	const uint32_t POWER_CALIBRATION_ADDRESS_UHF_400MHZ = 0x8F00B;									//UHF is in 5Mhz bands starting at 400Mhz.
+	const uint32_t POWER_CALIBRATION_ADDRESS_VHF_135MHZ = 0x8F07B;									//VHF is in 5Mhz bands from 135Mhz (Only first 8 entries are used)
+	int address;																					//first byte of each entry  is the low power and the second byte is the high power value
 	uint8_t buffer[2];
 
 
@@ -220,10 +214,10 @@ bool calibrationGetPowerForFrequency(int freq, calibrationPowerValues_t *powerSe
 	return true;
 }
 
-bool calibrationGetRSSIMeterParams(calibrationRSSIMeter_t *rssiMeterValues)
-{
-	int address;
-
+bool calibrationGetRSSIMeterParams(calibrationRSSIMeter_t *rssiMeterValues)							//Signal Level Meter setting
+{																									//two bytes per band
+	int address;																					//Low Byte is low end of meter range
+																									//High Byte is High End of Meter Range
 	if (trxCurrentBand[TRX_RX_FREQ_BAND] == RADIO_BAND_UHF)
 	{
 		address = 0x8F053;
