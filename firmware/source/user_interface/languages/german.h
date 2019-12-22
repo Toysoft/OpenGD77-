e2be39693ba687a4b33f420fdb59b226afbe2790
Daniel Caujolle-Bert <f1rmb.daniel@gmail.com>
2019-12-22 11:51:57 +0100

    Last german update from DG3GSP.

--- firmware/source/user_interface/languages/german.h
+++ firmware/source/user_interface/languages/german.h
@@ -20,7 +20,7 @@
  * Translators: DG3GSP, DL4LEX
  *
  *
- * Rev: 3
+ * Rev: 3.1
  */
 #ifndef USER_INTERFACE_LANGUAGES_GERMAN_H_
 #define USER_INTERFACE_LANGUAGES_GERMAN_H_
@@ -135,7 +135,7 @@ const stringsTable_t germanLanguage =
 .all_call				= "Ruf an alle", // MaxLen: 16
 .tone_scan				= "CTCSS Scan",//// MaxLen: 16
 .cc_scan				= "CC Scan",//// MaxLen: 16
-.low_battery			= "LOW BATTERY !!!",//// MaxLen: 16
+.low_battery			= "BATTERIE LEER!",//// MaxLen: 16
 };
 /********************************************************************
  *
