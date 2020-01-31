84994ddac7d2b728fcde2962f70d823d56fbd753
Daniel Caujolle-Bert <f1rmb.daniel@gmail.com>
2020-01-31 08:41:15 +0100

    New option to set source priority about contact lookups from Codeplug, DMRIDs DB and TalkerAlias. If Codeplug and DMRIDs lookups are failing, TA will be used as fallback, if any.

--- firmware/include/functions/fw_settings.h
+++ firmware/include/functions/fw_settings.h
@@ -29,6 +29,7 @@ enum USB_MODE { USB_MODE_CPS, USB_MODE_HOTSPOT, USB_MODE_DEBUG};
 enum SETTINGS_UI_MODE { SETTINGS_CHANNEL_MODE=0, SETTINGS_VFO_A_MODE, SETTINGS_VFO_B_MODE};
 enum BACKLIGHT_MODE { BACKLIGHT_MODE_AUTO = 0, BACKLIGHT_MODE_MANUAL = 1, BACKLIGHT_MODE_NONE = 2};
 enum HOTSPOT_TYPE { HOTSPOT_TYPE_OFF = 0, HOTSPOT_TYPE_MMDVM = 1, HOTSPOT_TYPE_BLUEDV = 2};
+enum CONTACT_DISPLAY_PRIO { CONTACT_DISPLAY_PRIO_CC_DB_TA = 0, CONTACT_DISPLAY_PRIO_DB_CC_TA, CONTACT_DISPLAY_PRIO_TA_CC_DB, CONTACT_DISPLAY_PRIO_TA_DB_CC };
 
 extern int settingsCurrentChannelNumber;
 extern bool settingsPrivateCallMuteMode;
@@ -73,7 +74,7 @@ typedef struct settingsStruct
 	uint32_t		vfoAScanHigh;                 //High frequency for VFO A Scanning
 	uint32_t		vfoBScanLow;                  //low frequency for VFO B Scanning
 	uint32_t		vfoBScanHigh;                 //High frequency for VFO B Scanning
-
+	uint8_t         contactDisplayPriority;
 
 } settingsStruct_t;
 
