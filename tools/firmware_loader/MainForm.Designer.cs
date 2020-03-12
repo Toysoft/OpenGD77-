8e162e1f8234291d59eb253b251827e396e074e4
Roger Clark <roger@rogerclark.net>
2020-03-12 15:42:25 +1100

    Minor GUI changes to GD77_FirmwareLoader

--- tools/firmware_loader/MainForm.Designer.cs
+++ tools/firmware_loader/MainForm.Designer.cs
@@ -38,30 +38,33 @@ namespace GD77_FirmwareLoader
 			// Model selector
 			//
 			this.grpboxModel = new System.Windows.Forms.GroupBox();
-			this.grpboxModel.Text = " Select Your Model ";
+			this.grpboxModel.Text = " Select your radio type";
 			this.grpboxModel.Location = new System.Drawing.Point(5, 5);
-			this.grpboxModel.Size = new System.Drawing.Size(130, 85);
+			this.grpboxModel.Size = new System.Drawing.Size(200, 85);
 			this.rbModels = new System.Windows.Forms.RadioButton[3];
 
 			this.rbModels[0] = new System.Windows.Forms.RadioButton();
-			this.rbModels[0].Text = "GD-77";
+			this.rbModels[0].Text = "Radioddity GD-77 / TYT MD-760";
 			this.rbModels[0].Location = new System.Drawing.Point(5, 15);
 			this.rbModels[0].UseVisualStyleBackColor = true;
 			this.rbModels[0].Tag = (int)FirmwareLoader.OutputType.OutputType_GD77;
+			this.rbModels[0].AutoSize = true;
 			this.rbModels[0].CheckedChanged += new System.EventHandler(this.rbModel_CheckedChanged);
 
 			this.rbModels[1] = new System.Windows.Forms.RadioButton();
-			this.rbModels[1].Text = "GD-77S";
+			this.rbModels[1].Text = "Radioddity GD-77S";
 			this.rbModels[1].Location = new System.Drawing.Point(5, 35);
 			this.rbModels[1].UseVisualStyleBackColor = true;
 			this.rbModels[1].Tag = (int)FirmwareLoader.OutputType.OutputType_GD77S;
+			this.rbModels[1].AutoSize = true;
 			this.rbModels[1].CheckedChanged += new System.EventHandler(this.rbModel_CheckedChanged);
 
 			this.rbModels[2] = new System.Windows.Forms.RadioButton();
-			this.rbModels[2].Text = "MD-1801";
+			this.rbModels[2].Text = "Baofeng MD-1801 / DM-860";
 			this.rbModels[2].Location = new System.Drawing.Point(5, 55);
 			this.rbModels[2].UseVisualStyleBackColor = true;
 			this.rbModels[2].Tag = (int)FirmwareLoader.OutputType.OutputType_DM1801;
+			this.rbModels[2].AutoSize = true;
 			this.rbModels[2].CheckedChanged += new System.EventHandler(this.rbModel_CheckedChanged);
 
 			this.rbModels[(int)FirmwareLoader.outputType].Checked = true;
@@ -74,9 +77,9 @@ namespace GD77_FirmwareLoader
 			// 
 			// btnOpenFile
 			// 
-			this.btnOpenFile.Location = new System.Drawing.Point(145, 67);
+			this.btnOpenFile.Location = new System.Drawing.Point(220, 67);
 			this.btnOpenFile.Name = "btnOpenFile";
-			this.btnOpenFile.Size = new System.Drawing.Size(144, 23);
+			this.btnOpenFile.AutoSize = true;
 			this.btnOpenFile.TabIndex = 0;
 			this.btnOpenFile.Text = "Select file and upload";
 			this.btnOpenFile.UseVisualStyleBackColor = true;
@@ -87,11 +90,11 @@ namespace GD77_FirmwareLoader
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(300, 100);
+			this.ClientSize = new System.Drawing.Size(380, 100);
 			this.Controls.Add(this.btnOpenFile);
 			this.KeyPreview = true;
 			this.Name = "MainForm";
-			this.Text = "Open(GD77/MD1801) Firmware loader";
+			this.Text = "Open(GD77/GD77S/MD1801) Firmware loader";
 			this.FormBorderStyle = FormBorderStyle.FixedSingle;
 			this.MaximizeBox = false;
 			this.ResumeLayout(false);
